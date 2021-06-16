0ba03c9a3f1781e18b54a9fa9bb23f1d385b4708
kasunlk <bkasun556@gmail.com>
2021-06-16 16:56:31 +0000

    create functions for handle checkboxes in course registration

--- src/controller/CourseRegistrationController.java
+++ src/controller/CourseRegistrationController.java
@@ -2,10 +2,8 @@ package controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.event.Event;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -30,18 +28,35 @@ public class CourseRegistrationController {
     public AnchorPane name;
     public Label lblDate;
     public Label lbltime;
+    public Label lblStudentId;
+    public ImageView lblAttachPhoto;
+    public CheckBox chkPartTime;
+    public CheckBox chkFullTime;
     //private ImageView ImageView;
 
 
     public void initialize(){
+        initWindow();
+    }
+
+    private void initWindow(){
         MaterialUI.paintComboBox(cmbCourse,cmbQualification);
         MaterialUI.paintTextFields(txtName,txtPlacementExamMarks,txtAddress,txtCommencementDate,txtDateOfBirth,txtEmail,txtPhoto);
         MaterialUI.checkBox(txtName,txtPlacementExamMarks,txtAddress,txtCommencementDate,txtDateOfBirth,txtEmail);
         MaterialUI.drawBorder(pneBody);
         lblDate.setText(DateAndTime.DateToday());
+        checkBoxControl(chkPartTime,chkFullTime);
+        checkBoxControl(chkFullTime,chkPartTime);
     }
-
-
+    private void checkBoxControl(CheckBox clickedBox,CheckBox disableBox){
+        clickedBox.setOnMouseClicked(event -> {
+            if(disableBox.isDisable()){
+                disableBox.setDisable(false);
+            }else{
+                disableBox.setDisable(true);
+            }
+        });
+    }
 
     public void btnClearAll_OnAction(ActionEvent actionEvent) {
     }
@@ -57,4 +72,10 @@ public class CourseRegistrationController {
     public void lblAttachPhoto_OnMouseClicked(MouseEvent mouseEvent) {
     }
 
+    public void chkPartTime_OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void chkFullTime_OnMouseClocked(MouseEvent mouseEvent) {
+    }
 }
