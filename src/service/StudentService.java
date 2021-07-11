f63f1b220f271a61a78ec57522a33fd0abe8cd10
kasunlk <bkasun556@gmail.com>
2021-07-11 08:39:55 +0000

    finalized business validations in student service class

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,6 +1,8 @@
 package service;
 
 import model.Student;
+import service.exception.DuplicateEntryException;
+import service.exception.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,18 +10,20 @@ import java.util.List;
 public class StudentService {
     private static List<Student> studentDB = new ArrayList<>();
 
-    public static void saveStudent(Student student){
-
+    public static void saveStudent(Student student) throws DuplicateEntryException {
+        if(isExist(student.getNic())){
+            throw new DuplicateEntryException();
+        }
         studentDB.add(student);
     }
 
-    public static void updateStudent(Student student){
+    public static void updateStudent(Student student) throws NotFoundException{
         Student s = findStudent(student.getNic());
         int index = studentDB.indexOf(s);
         studentDB.set(index, student);
     }
 
-    public static void deleteStudent(String nic){
+    public static void deleteStudent(String nic) throws NotFoundException{
         Student student = findStudent(nic);
         studentDB.remove(student);
     }
@@ -28,13 +32,24 @@ public class StudentService {
         return studentDB;
     }
 
-    public static Student findStudent(String nic){
+    private static boolean isExist(String nic){
+
         for(Student student : studentDB){
+            if(student.getNic().equals(nic)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Student findStudent(String nic) throws NotFoundException{
+        for(Student student : studentDB){
+
             if(student.getNic().equals(nic)){
                 return student;
             }
         }
-        return null;
+        throw new NotFoundException();
     }
 
     public static List<Student> findStudents(String query){
---
+++ src/service/exception/DuplicateEntryException.java
@@ -0,0 +1,5 @@
+package service.exception;
+
+public class DuplicateEntryException extends Exception{
+
+}
